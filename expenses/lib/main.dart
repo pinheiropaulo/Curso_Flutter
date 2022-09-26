4c950949adb592fb4a73b2dece835278e4ab4dee
Paulo Pinheiro <pauloprap57@gmail.com>
2022-09-26 14:17:53 -0300

    Verão Inicial

---
+++ expenses/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas Pessoais"),
+      ),
+      body: const Center(
+        child: Text("Verão Inicial"),
+      ),
+    );
+  }
+}
